9579acc44f07bf81bebc38170485c96b9bd04ec2
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-07-07 17:34:38 +0000

    Added getRootDomain function

--- utils.js
+++ utils.js
@@ -1,17 +1,15 @@
-export async function getRootDomain(url){
+export async function getRootDomain(url, browser){
   try {
-    await fetch(url,{
-      method: 'GET',
-      redirect: 'follow'
-    })
-    const finalUrl = response.url;
-    return finalUrl;
+    const rootPage = await browser.newPage()
+    await rootPage.goto(url)
+    const rootUrl = rootPage.url()
+    await rootPage.close()
+    return rootUrl
   } catch (error) {
-    
+    console.error(error.message);
+    throw Error()
   }
 }
 
-getRootdomain('https://www.yelp.com/biz_redir?url=https%3A%2F%2Fdoubleeroofingaz.com&cachebuster=1720367795&website_link_type=website&src_bizid=f-_Z0eARPk-5d-F0OR4EWQ&s=21c037a14c912ad761b024e62b7f3e6f9b8ef45e017cb88d4f291b7fd6e1adc5').then((result)=>{
-  console,log(result)
-})
+
 
